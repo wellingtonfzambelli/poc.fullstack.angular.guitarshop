3a897493aeb3384313ddb2082c9e7b8f13b23426
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-12-04 15:42:11 -0300

    add header component add product content

---
+++ poc.fullstack.angular.guitarshop.frontend/src/app/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
