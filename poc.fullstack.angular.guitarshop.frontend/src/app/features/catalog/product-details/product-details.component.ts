5fff492a2374ca819e9f32fc1183d35b3d4057df
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-12-08 18:22:01 -0300

    finishing cart logic

--- poc.fullstack.angular.guitarshop.frontend/src/app/features/catalog/product-details/product-details.component.ts
+++ poc.fullstack.angular.guitarshop.frontend/src/app/features/catalog/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { Product } from '../../../shared/models/Product';
 import { GuitarShopService } from '../../../core/services/guitar-shop.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,6 +8,10 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartItem } from '../../../shared/models/Cart';
+import { environment } from '../../../../environments/environment';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -18,7 +22,8 @@ import { MatDivider } from '@angular/material/divider';
     MatFormField,
     MatInput,
     MatLabel,
-    MatDivider
+    MatDivider,
+    FormsModule
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
@@ -27,8 +32,15 @@ export class ProductDetailsComponent implements OnInit {
   // get from querystring
 
   public product?: Product;
-  
-  constructor(private guitarShopService:GuitarShopService, private activeRoute: ActivatedRoute) {
+  public item = input.required<CartItem>();
+  public quantityInCart: number = 0;
+  public quantityInitial = 0;
+  public productImagePath = environment.imageProductPath;  
+
+  constructor(
+    private guitarShopService:GuitarShopService, 
+    private activeRoute: ActivatedRoute,
+    public cartService: CartService) {
 
   }
 
@@ -36,15 +48,45 @@ export class ProductDetailsComponent implements OnInit {
    this.loadProduct();
   }
 
-  loadProduct() {
+  public loadProduct() {
     const id = this.activeRoute.snapshot.paramMap.get('id');
     
     if(!id)
       return;
 
     this.guitarShopService.getProductById(id).subscribe({
-      next: response => this.product = response,
+      next: response => {
+        this.product = response;
+        this.updateQuantityInCart();
+      },
       error: error => console.log(error)
     })
+  }
+
+  public updateQuantityInCart() {
+    this.quantityInCart = this.cartService.cart()?.items
+      .find(x => x.productId === this.product?.id)?.quantity || 0
+
+    this.quantityInitial = this.quantityInCart || 1;
+  }
+
+  public updateCart() {
+    if(!this.product)
+      return;
+
+    if(this.quantityInitial > this.quantityInCart){
+      const itemsToAdd = this.quantityInitial - this.quantityInCart;
+
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemtoCart(this.product, itemsToAdd);
+    }else{
+      const itemsToRemove = this.quantityInCart - this.quantityInitial;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
+  }
+
+  public changeButtonText() {
+    return this.quantityInCart > 0 ? 'Update cart' : 'update cart';
   }
 }
