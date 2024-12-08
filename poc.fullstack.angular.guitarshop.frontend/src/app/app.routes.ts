e3c7300fa9961265af2474ccca24d9abe4caa01e
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-12-08 18:25:26 -0300

    starting checkout

--- poc.fullstack.angular.guitarshop.frontend/src/app/app.routes.ts
+++ poc.fullstack.angular.guitarshop.frontend/src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'catalog/:id', component: ProductDetailsComponent },
     { path: 'cart', component: CartComponent },
+    { path: 'checkout', component: CheckoutComponent },
     { path: 'contact', component: ContactComponent },
     
     { path: 'test-error', component: TestErrorComponent },
