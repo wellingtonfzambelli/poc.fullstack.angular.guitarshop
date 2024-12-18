2594c33a928236e4ff25be319b1e9a61ff8832a2
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-12-18 16:55:28 -0300

    creating a footer

--- poc.fullstack.angular.guitarshop.frontend/src/app/app.component.ts
+++ poc.fullstack.angular.guitarshop.frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./layout/footer/footer.component";
 import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
